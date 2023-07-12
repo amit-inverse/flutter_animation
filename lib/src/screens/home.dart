ef322c5ac7bd821eae01391b18ac0f93b3785c48
Amit Kumar Das <amit.inverse@gmail.com>
2023-07-12 22:39:29 +0530

    Tweens with Curves

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -20,6 +20,10 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
       duration: const Duration(seconds: 2),
       vsync: this,
     );
+    catAnimation = Tween(begin: 0.0, end: 100.0).animate(CurvedAnimation(
+      parent: catController,
+      curve: Curves.easeIn,
+    ));
   }
 
   @override
