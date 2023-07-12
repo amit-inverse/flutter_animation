e7db4cb8ce78159c4fc3c7ba9b276251272b72d7
Amit Kumar Das <amit.inverse@gmail.com>
2023-07-12 07:45:37 +0530

    Stateful Widget for Animation

---
+++ lib/src/screens/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  @override
+  Widget build(context) {
+    return const Text('HOME');
+  }
+}
